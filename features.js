261141c490e845c3c7665f6a9ca7d270209ad4d7
Nipurn Bhaal <nbhaal0805@gmail.com>
2023-08-24 14:42:25 +0530

    initial commit

---
+++ features.js
@@ -0,0 +1,10 @@
+// const gfName = "MrsEllen";
+// const gfName2 = "MrsEllen2";
+// const gfName3 = "MrsEllen3";
+
+// export default gfName;
+// export { gfName2, gfName3 };
+
+export const generateLovePercent = () => {
+  return `${~~(Math.random() * 100)}%`;
+};
